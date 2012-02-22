4d315cdd5150c4cc3ec9b8d7c753cee92efe9e47
Matthew Strickland <mpstrick@gmail.com>
2012-02-22 13:55:26 -0500

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitnesstracker::Application.config.secret_token = '13d00cddeb82c9b4b699fea14e63fcc92a56bb4eccabb403ee69d6e44f0c65fcaa91efb5937f402112dd8afe9ef95149651313ac02d67290c87ccb3d8f83b913'
